e0314f466623eb4e9e53ac97c0622b386139c252
ichsnn <ichsan.nulmuhlis@gmail.com>
2023-11-29 09:27:46 +0700

    update

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ const fixBug = "This is a fix bug"
 const b = "This is feature a from main branch"
 
 const featureB = "This is feature b from main branch with rebase"
-<<<<<<< HEAD
-=======
 const featureC = "This is feature c"
->>>>>>> d56f153b8ce2ca17c070d83b98ac7006f3f57fd5
 const featureD = "This is feature D"
 
 console.log(a)
