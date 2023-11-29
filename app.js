c96251e504c3f9491b72bdb04940ca8b7ca80c34
ichsnn <ichsan.nulmuhlis@gmail.com>
2023-11-29 09:16:37 +0700

    feat: feature d

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const fixBug = "This is a fix bug"
 const b = "This is feature a from main branch"
 
 const featureB = "This is feature b from main branch with rebase"
+const featureD = "This is feature D"
 
 console.log(a)
 console.log(b)
 console.log(c)
+console.log(featureD)
